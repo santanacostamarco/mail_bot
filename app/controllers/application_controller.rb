bea8b967c0997ad22e0ea8d9fb27dfb40799d154
Marco Antonio <santannacostamarco@gmail.com>
2017-12-13 17:55:28 -0200

    Imap config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def email_checker
+    require 'mail'
+    Mail.defaults do
+      retriever_method :imap,
+        :address => "imap.gmail.com",
+        :port => 993,
+        :user_name => "botnovahub@gmail.com",
+        :password => "B4l3$tr4",
+        :enable_ssl => true,
+        :read_only => false
+    end
+
+    emails = Mail.find(keys: ["SUBJECT", "*"]) # keys: ["NOT", "SEEN"]
+
+    render html: emails.length
+
+    
+  end
 end
