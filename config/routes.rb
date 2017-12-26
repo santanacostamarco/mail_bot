85d951c0bdf4b67e46411dead5633c8e363004aa
Marco Antonio <santannacostamarco@gmail.com>
2017-12-26 17:19:04 -0200

    removing EmailsController and resources from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :emails
+  #resources :emails
   resources :mail
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "mail#show"
+
+  get '/main', to: 'mail#main'
 end
